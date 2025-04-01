aae46a43ab4dd09fcf26b1b59476a483093d60ad
Devesh <deveshparyani17@gmail.com>
2025-04-01 14:59:30 +0530

    Make Changes

--- backend/dist/webscraper.js
+++ backend/dist/webscraper.js
@@ -256,7 +256,7 @@ async function aiCall(reviews) {
     }
 }
 async function getDetails(productURL) {
-    const browser = await puppeteer_extra_1.default.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer_extra_1.default.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
     await page.setViewport({ width: 1280, height: 800 });
--- backend/src/webscraper.ts
+++ backend/src/webscraper.ts
@@ -317,7 +317,7 @@ interface ReviewSummaryType {
 }
 
 export async function getDetails(productURL) {
-    const browser = await puppeteer.launch({ headless: "new" as any, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
